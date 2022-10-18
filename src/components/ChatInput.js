f8c9fb6b1e92831c5a3e5c7769c8b981326da5a6
Alyna Palamarchuk <alynapchuk@gmail.com>
2022-10-18 10:00:04 -0500

    update logo

--- src/components/ChatInput.js
+++ src/components/ChatInput.js
@@ -1,6 +1,11 @@
+import { useState } from "react"
 const ChatInput = () => {
+    const [textArea, setTextArea] = useState(null)
     return (
-        <div></div>
+        <div className="chat-input">
+            <textarea value={textArea} onChange={(e) => setTextArea(e.target.value)} />
+            <button className="secondary-button">Submit</button>
+        </div>
     )
 }
 
